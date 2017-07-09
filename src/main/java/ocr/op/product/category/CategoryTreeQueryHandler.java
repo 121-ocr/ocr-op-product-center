c47151803e1238e7608fb9366c32bed0eb1b4352
lijing <njlijing1024@163.com>
2017-07-09 23:56:46 +0800

    修改产品分类树
    Signed-off-by: lijing <njlijing1024@163.com>

--- src/main/java/ocr/op/product/category/CategoryTreeQueryHandler.java
+++ src/main/java/ocr/op/product/category/CategoryTreeQueryHandler.java
@@ -4,7 +4,6 @@ import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import otocloud.common.ActionURI;
-import otocloud.common.OtoCloudDirectoryHelper;
 import otocloud.framework.app.function.ActionDescriptor;
 import otocloud.framework.app.function.ActionHandlerImpl;
 import otocloud.framework.app.function.AppActivityImpl;
